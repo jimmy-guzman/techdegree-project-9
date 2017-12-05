772716d4b8ec43f7f217bd3effedc7f90ed70b13
Jimmy Guzman <jimmy.guzman@outlook.com>
2017-12-05 14:23:01 -0600

    add notification popup

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
+
+const alert = document.querySelector(".alert");
+const alertButton = document.getElementById("alertButton");
+const messageUser = document.querySelector(".message-user");
+const form = messageUser.getElementsByTagName("form")[0];
+const user = document.getElementById("user");
+const message = document.getElementById("message");
+const sendButton = document.getElementById("sendButton");
+const emailNotifSwitch = document.getElementById("emailNotifSwitch");
+const profilePublicSwitch = document.getElementById("profilePublicSwitch");
+const timeZone = document.getElementById("timeZone");
+const saveButton = document.getElementById("saveButton");
+const cancelButton = document.getElementById("cancelButton");
+let saveSettings;
+const ul = document.getElementById("autocompleteNames");
+const notification = document.querySelector('header').querySelector('.notification');
+const popUp = document.querySelector('.pop-up');
+const notifications = popUp.querySelector('.notifications');
+const poUpbutton = document.getElementById("closeNotifButton");
+
+
+// ----- chart js set up -----
 var ctx = document.getElementById("dailyTrafficChart").getContext("2d");
 var dailyTrafficChart = new Chart(ctx, {
   type: "bar",
   data: {
     labels: ["S", "M", "T", "W", "T", "F", "S"],
-    datasets: [
-      {
-        label: "",
-        data: [50, 75, 150, 100, 200, 175, 75],
-        backgroundColor: "rgba(116, 119, 191, 1)"
-      }
-    ]
+    datasets: [{
+      label: "",
+      data: [50, 75, 150, 100, 200, 175, 75],
+      backgroundColor: "rgba(116, 119, 191, 1)"
+    }]
   },
   options: {
     legend: {
@@ -19,26 +39,22 @@ var dailyTrafficChart = new Chart(ctx, {
       padding: 20
     },
     scales: {
-      xAxes: [
-        {
-          barPercentage: 0.6,
-          gridLines: {
-            drawTicks: true
-          }
+      xAxes: [{
+        barPercentage: 0.6,
+        gridLines: {
+          drawTicks: true
         }
-      ],
-      yAxes: [
-        {
-          gridLines: {
-            drawTicks: true
-          },
-          ticks: {
-            stepSize: 50,
-            max: 250,
-            min: 0
-          }
+      }],
+      yAxes: [{
+        gridLines: {
+          drawTicks: true
+        },
+        ticks: {
+          stepSize: 50,
+          max: 250,
+          min: 0
         }
-      ]
+      }]
     }
   }
 });
@@ -48,13 +64,11 @@ var mobileUsersChart = new Chart(cty, {
   type: "doughnut",
   data: {
     labels: ["Tablets", "Phones", "Desktop"],
-    datasets: [
-      {
-        data: [15, 15, 70],
-        backgroundColor: ["#81c98f", "#74b1bf", "#7477bf"],
-        borderWidth: [0, 0, 0]
-      }
-    ]
+    datasets: [{
+      data: [15, 15, 70],
+      backgroundColor: ["#81c98f", "#74b1bf", "#7477bf"],
+      borderWidth: [0, 0, 0]
+    }]
   },
   options: {
     legend: {
@@ -89,31 +103,29 @@ var trafficChart = new Chart(ctz, {
       "November",
       "December"
     ],
-    datasets: [
-      {
-        label: "",
-        data: [
-          750,
-          1250,
-          750,
-          1250,
-          1750,
-          1250,
-          1500,
-          1000,
-          1500,
-          2000,
-          250,
-          1800
-        ],
-        backgroundColor: "rgba(116, 119, 191, .25)",
-        borderColor: "#7477bf",
-        borderWidth: 1,
-        lineTension: 0,
-        pointBackgroundColor: "white",
-        pointRadius: 5
-      }
-    ]
+    datasets: [{
+      label: "",
+      data: [
+        750,
+        1250,
+        750,
+        1250,
+        1750,
+        1250,
+        1500,
+        1000,
+        1500,
+        2000,
+        250,
+        1800
+      ],
+      backgroundColor: "rgba(116, 119, 191, .25)",
+      borderColor: "#7477bf",
+      borderWidth: 1,
+      lineTension: 0,
+      pointBackgroundColor: "white",
+      pointRadius: 5
+    }]
   },
   options: {
     legend: {
@@ -128,56 +140,39 @@ var trafficChart = new Chart(ctz, {
       }
     },
     scales: {
-      xAxes: [
-        {
-          gridLines: {
-            drawTicks: true,
+      xAxes: [{
+        gridLines: {
+          drawTicks: true,
 
-            offsetGridLines: false
-          }
+          offsetGridLines: false
         }
-      ],
+      }],
 
-      yAxes: [
-        {
-          gridLines: {
-            drawTicks: true,
-            offsetGridLines: false
-          },
-          ticks: {
-            min: 0,
-            stepSize: 500,
-            max: 2500
-          }
+      yAxes: [{
+        gridLines: {
+          drawTicks: true,
+          offsetGridLines: false
+        },
+        ticks: {
+          min: 0,
+          stepSize: 500,
+          max: 2500
         }
-      ]
+      }]
     }
   }
 });
 
-const alert = document.querySelector(".alert");
-const alertButton = document.getElementById("alertButton");
-const messageUser = document.querySelector(".message-user");
-const form = messageUser.getElementsByTagName("form")[0];
-const user = document.getElementById("user");
-const message = document.getElementById("message");
-const sendButton = document.getElementById("sendButton");
-const emailNotifSwitch = document.getElementById("emailNotifSwitch");
-const profilePublicSwitch = document.getElementById("profilePublicSwitch");
-const timeZone = document.getElementById("timeZone");
-const saveButton = document.getElementById("saveButton");
-const cancelButton = document.getElementById("cancelButton");
-let saveSettings;
-const ul = document.getElementById("autocompleteNames");
+
 //----- close alert -----
-alertButton.addEventListener("click", function() {
+alertButton.addEventListener("click", function () {
   alert.style.display = "none";
 });
 
 //-----  error messages and sent confirmation simulator (needs refactoring) -----
-sendButton.addEventListener("click", function() {
+sendButton.addEventListener("click", function () {
   if (!user.value || !message.value) {
-    user.addEventListener("input", function(e) {
+    user.addEventListener("input", function (e) {
       sendButton.innerText = "Send";
       sendButton.style.backgroundColor = "#7477bf";
       if (this.value) {
@@ -186,7 +181,7 @@ sendButton.addEventListener("click", function() {
         this.style.borderColor = "red";
       }
     });
-    message.addEventListener("input", function(e) {
+    message.addEventListener("input", function (e) {
       sendButton.innerText = "Send";
       sendButton.style.backgroundColor = "#7477bf";
       if (this.value) {
@@ -216,85 +211,108 @@ sendButton.addEventListener("click", function() {
   }
 });
 
-// ----- save and cancel settings (needs refactoring) -----
+// ----- save and cancel settings  -----
 
+let settings = {
+  load: function () {
+    timeZone.selectedIndex = localStorage.selectedIndex;
+    emailNotifSwitch.checked = JSON.parse(localStorage.sendEmail);
+    profilePublicSwitch.checked = JSON.parse(localStorage.setProfile);
+  },
+  setUpEventListeners: function () {
+    saveButton.addEventListener("click", function () {
+      localStorage.saveSettings = true;
+      localStorage.selectedIndex = timeZone.selectedIndex;
+      localStorage.sendEmail = emailNotifSwitch.checked;
+      localStorage.setProfile = profilePublicSwitch.checked;
+    });
+    cancelButton.addEventListener("click", function () {
+      settings.load();
+    });
+  }
+}
+
+settings.setUpEventListeners();
 if (localStorage.saveSettings) {
-  loadSettings();
+  settings.load();
 }
 
-function loadSettings() {
-  timeZone.selectedIndex = localStorage.selectedIndex;
-  emailNotifSwitch.checked = JSON.parse(localStorage.sendEmail);
-  profilePublicSwitch.checked = JSON.parse(localStorage.setProfile);
-}
+// ----- autocomplete function -----
 
-saveButton.addEventListener("click", function() {
-  localStorage.saveSettings = true;
-  localStorage.selectedIndex = timeZone.selectedIndex;
-  localStorage.sendEmail = emailNotifSwitch.checked;
-  localStorage.setProfile = profilePublicSwitch.checked;
-});
 
-cancelButton.addEventListener("click", function() {
-  loadSettings();
-});
-
-// ----- autocomplete function (needs refactoring) -----
-let users = [
-  "Victoria Chambers",
-  "Dale Byrd",
-  "Dawn Wood",
-  "Dan Oliver",
-  "Ricardo Trautman",
-  "Alfredo Debelak",
-  "Benny Medlin",
-  "Tiera Ollis",
-  "Vanita Chevere",
-  "Shiloh Clarkson",
-  "Maryann Krull",
-  "Rachel Lain",
-  "Veola Christofferso",
-  "Catherin Kifer",
-  "Vergie Templin",
-  "France Mackley",
-  "Ethelyn Nourse",
-  "Gloria Hochman",
-  "Daren Drinkwater",
-  "Emelina Gulbranson",
-  "Cristin Weil",
-  "Jessica Hopton",
-  "Amada Champney",
-  "Shan Seldon"
-];
-
-function searchUsers(arr, str) {
-  let results = [];
-  for (let i = 0; i < arr.length; i++) {
-    let temp = users[i].slice(0, str.length);
-    if (str === temp) {
-      results.push(users[i]);
+let autoComplete = {
+  users: [
+    "Victoria Chambers",
+    "Dale Byrd",
+    "Dawn Wood",
+    "Dan Oliver",
+    "Ricardo Trautman",
+    "Alfredo Debelak",
+    "Benny Medlin",
+    "Tiera Ollis",
+    "Vanita Chevere",
+    "Shiloh Clarkson",
+    "Maryann Krull",
+    "Rachel Lain",
+    "Veola Christofferso",
+    "Catherin Kifer",
+    "Vergie Templin",
+    "France Mackley",
+    "Ethelyn Nourse",
+    "Gloria Hochman",
+    "Daren Drinkwater",
+    "Emelina Gulbranson",
+    "Cristin Weil",
+    "Jessica Hopton",
+    "Amada Champney",
+    "Shan Seldon"
+  ],
+  searchUsers: function (arr, str) {
+    let results = [];
+    for (let i = 0; i < arr.length; i++) {
+      let temp = autoComplete.users[i].slice(0, str.length);
+      if (str.toLowerCase() === temp.toLowerCase()) {
+        results.push(autoComplete.users[i]);
+      }
     }
+    autoComplete.render(results);
+  },
+  render: function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+      ul.innerHTML += "<li>" + arr[i] + "</li>";
+    }
+  },
+  setUpEventListeners: function () {
+    user.addEventListener("input", function (e) {
+      let input = this.value;
+      if (this.value.length > 0) {
+        ul.innerHTML = "";
+        autoComplete.searchUsers(autoComplete.users, input);
+      } else {
+        ul.innerHTML = "";
+      }
+    });
+    ul.addEventListener("click", function (e) {
+      user.value = e.target.innerText;
+      this.innerHTML = "";
+    });
   }
-  return results;
 }
 
-user.addEventListener("input", function(e) {
-  let input = this.value;
-  if (this.value.length > 0) {
-    ul.innerHTML = "";
-    render(searchUsers(users, input));
-  } else {
-    ul.innerHTML = "";
-  }
-});
+autoComplete.setUpEventListeners();
 
-function render(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    ul.innerHTML += "<li>" + arr[i] + "</li>";
+notification.addEventListener('click', function(){
+  popUp.style.display = 'flex';
+  let notifs = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.', 'Praesent libero. Sed cursus ante dapibus diam. Sed nisi.' ];
+  notifications.innerHTML = '';
+  for (let i = 0; i < notifs.length; i++) {
+    let newNotif= (document.createElement('P'));
+    newNotif.innerText = notifs[i];
+    notifications.appendChild(newNotif);
   }
-}
+})
 
-ul.addEventListener("click", function(e) {
-  user.value = e.target.innerText;
-  ul.innerHTML = "";
-});
+closeNotifButton.addEventListener('click', function () {
+  popUp.style.display = 'none';
+
+})
